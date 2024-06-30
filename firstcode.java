87ef39db940a6b4275ab8f6a3e7893c05fd17983
karthik <kartikkatti55@gmail.com>
2024-06-30 23:40:09 +0530

    book

--- firstcode.java
+++ firstcode.java
@@ -4,6 +4,27 @@
 public class firstcode {
         public static void main(String[] args) {
             System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
+            System.out.println("HI zomato");
 
         }
     
